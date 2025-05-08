ed022a216c9d937f1f2f0e02d102011422155434
vikneshoftheleaf <vikneshoftheleaf@gmail.com>
2025-05-08 12:19:18 +0530

    env

--- src/firebase.js
+++ src/firebase.js
@@ -9,7 +9,7 @@ import { getFirestore } from "firebase/firestore";
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
     apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
-    authDomain: process.NEXT_PUBLIC_env.FIREBASE_AUTH_DOMAIN,
+    authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
     projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
     storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
     messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
